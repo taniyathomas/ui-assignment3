df423898f15f7e17d8188b0a860a83dabf3f32ba
taniyathomas <taniya.thomas@mtxb2b.com>
2020-11-08 11:12:18 +0530

    Second commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function toggleSidebar(){
+    document.getElementById("sidebar").classList.toggle('active');
+}
+function toggle_menu(){
+     document.getElementById("mobile-menu").classList.toggle('disp');    
+}
