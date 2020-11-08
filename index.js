2daf7112f39b4f10cbad8800d525aea4a5686721
taniyathomas <taniya.thomas@mtxb2b.com>
2020-11-08 15:02:03 +0530

    Modified style

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function toggleSidebar(){
     document.getElementById("sidebar").classList.toggle('active');
 }
-function toggle_menu(){
-     document.getElementById("mobile-menu").classList.toggle('disp');    
-}
+
+
+
+
